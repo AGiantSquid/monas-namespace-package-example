73268fa90a2852890416db86bbe6d17b88b27087
Ashley <ashley.e.shultz@gmail.com>
2023-08-04 07:49:51 -0700

    first commit

---
+++ acme-lib-a/src/acme/lib_a/__init__.py
@@ -0,0 +1,7 @@
+from acme.lib_b import func_b
+
+def func_a():
+    print('hello from lib_a func_a')
+    func_b()
+
+func_a()
---
+++ acme-lib-c/src/acme/lib_c/__init__.py
@@ -0,0 +1,2 @@
+def func_c():
+    print('hello from lib_c func_c, edit')
